0e85a693b9fbf257f2f27fac288890bd855d8e2e
dell <223110142@upmh.edu.mx>
2025-11-13 13:18:14 -0600

    Proyecto con mi carpeta de imagenes, y mis dos interfaces .dart

---
+++ menu/lib/main.dart
@@ -0,0 +1,449 @@
+import 'package:flutter/material.dart';
+import 'package:menu/terminos.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplicacion para niños',
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/fondoMain.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10, left: 30, right: 30),
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 244, 178, 72),
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  width: 3,
+                  color: Color.fromARGB(255, 244, 178, 72), //
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          print('sirve foto');
+                        },
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage('images/gatochat.png'),
+                              fit: BoxFit.cover,
+                            ),
+                            border: Border.all(width: 2, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Niño',
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print('opcion nivel 1');
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 8,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 79, 195, 247), // Azul claro
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          width: 2,
+                          color: Color.fromARGB(255, 79, 195, 247),
+                        ),
+                      ),
+                      child: Text(
+                        'Nivel 1',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('funciona apartado del bombero');
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 50, right: 50, top: 20),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 250, 120, 50), // Naranja
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    width: 1,
+                    color: Color.fromARGB(255, 250, 120, 50),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 180,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/bomberito.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 30),
+                    Expanded(
+                      child: Text(
+                        '"Pequeños héroes, grandes acciones"',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // cuadros de juegos
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                child: Row(
+                  children: [
+                    // Columna izquierda
+                    Expanded(
+                      child: Column(
+                        children: [
+                          // Juego 1
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                print('sirve juegos 1');
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color.fromARGB(144, 0, 0, 0),
+                                      offset: Offset(1, 7),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            image: AssetImage(
+                                              'images/juego1.png',
+                                            ),
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(12),
+                                      child: Text(
+                                        'juegos1',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Juego 3
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                print('sirve juegos 3');
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(top: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color.fromARGB(144, 0, 0, 0),
+                                      offset: Offset(1, 7),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            image: AssetImage(
+                                              'images/juego3.png',
+                                            ),
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(12),
+                                      child: Text(
+                                        'juegos3',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    // Columna derecha
+                    Expanded(
+                      child: Column(
+                        children: [
+                          // Juego 2
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                print('sirve juegos 2');
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color.fromARGB(144, 0, 0, 0),
+                                      offset: Offset(1, 7),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            image: AssetImage(
+                                              'images/juego2.png',
+                                            ),
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(12),
+                                      child: Text(
+                                        'juegos2',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Juego 4
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                print('sirve juegos 4');
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(top: 10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color.fromARGB(144, 0, 0, 0),
+                                      offset: Offset(1, 7),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                        margin: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            image: AssetImage(
+                                              'images/juego4.png',
+                                            ),
+                                            fit: BoxFit.contain,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(12),
+                                      child: Text(
+                                        'juegos4',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Container(
+              height: 70,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 138, 234, 255),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // Icono 1
+                  GestureDetector(
+                    onTap: () {
+                      print('sirve el de menu');
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 2, color: Colors.white),
+                      ),
+                      child: Icon(Icons.menu, color: Colors.white, size: 40),
+                    ),
+                  ),
+                  // Icono 2
+                  GestureDetector(
+                    onTap: () {
+                      print('sirve el de perfil');
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 2, color: Colors.white),
+                      ),
+                      child: Icon(Icons.person, color: Colors.white, size: 40),
+                    ),
+                  ),
+                  // Icono 3
+                  GestureDetector(
+                    onTap: () {
+                      print('sirve el de configuracion');
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.orangeAccent,
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 2, color: Colors.white),
+                      ),
+                      child: Icon(
+                        Icons.settings,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ menu/lib/terminos.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class TerminosScreen extends StatelessWidget {
+  const TerminosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/imgTerminos.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.95),
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 6,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('images/logoBomberos.png'),
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'MANITAS SIN FUEGO',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.black87,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 4),
+                        const Text(
+                          'TÉRMINOS Y CONDICIONES DE USO',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black54,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        const Text(
+                          '1. Aceptación de los términos:\n'
+                          'Al descargar, acceder o utilizar esta aplicación, usted (el padre, madre o tutor legal del menor) acepta los presentes Términos y Condiciones. '
+                          'Si no está de acuerdo con alguno de los puntos aquí establecidos, le pedimos que no permita que el menor utilice la aplicación.\n\n'
+                          '2. Uso bajo supervisión adulta:\n'
+                          'Esta aplicación está diseñada para ser utilizada por niños con la supervisión y consentimiento de un adulto responsable. '
+                          'El adulto es el encargado de leer y explicar el contenido al niño y de vigilar su interacción con los juegos o materiales de la app.\n\n'
+                          '3. Propósito educativo:\n'
+                          'Manitas sin fuego no se hace responsable del mal uso que se haga del contenido de la aplicación fuera de su contexto educativo. '
+                          'Cualquier práctica con o cerca de fuego debe realizarse únicamente bajo la guía de un adulto y en condiciones seguras.\n\n'
+                          '4. Recordatorio importante para padres o tutores:\n'
+                          'Esta aplicación no reemplaza la enseñanza directa ni la supervisión adulta. Su propósito es reforzar hábitos de seguridad y prevención del fuego '
+                          'mediante el juego y el aprendizaje interactivo.\n',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black87,
+                            height: 1.5,
+                          ),
+                          textAlign: TextAlign.justify,
+                        ),
+                        const SizedBox(height: 30),
+
+                        // Botón de aceptar
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.redAccent,
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              elevation: 4,
+                            ),
+                            onPressed: () {
+                              // 👇 Aquí puedes regresar a la pantalla anterior
+                              Navigator.pop(context);
+                            },
+                            child: const Text(
+                              'ACEPTO LOS TÉRMINOS Y CONDICIONES',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
